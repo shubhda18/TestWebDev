4052867f4099a54c7fe6f9a1ea71ee677b4d5db3
Shubhda Datta <>
2022-09-27 11:55:44 +0000

    Added Flask and rendered templates

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def html_webpage():
+  return render_template('home.html')
+  print(__name__)
+
+if __name__=="__main__":
+  app.run(host="0.0.0.0", debug = True)
