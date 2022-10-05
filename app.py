18715132e8e0150f0566ae46b85eda83ec18fee3
Shubhda Datta <106992849+shubhda18@users.noreply.github.com>
2022-10-05 14:41:27 +0000

    Added Projects ,Certificates data and JSON Api

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 Projects =[
   {"id":1,
   "Title":"Webscraping TMDB",
-  "Link":" "},
+   "Summary":" ",  "Link":"https://colab.research.google.com/drive/1O4AU7aLXRKzinKcT1k3VP6lty-0Rqtnt?usp=sharing"},
+  
   {"id":2,
   "Title":"Medical Retinopathy Image Classification-Pytorch",
+   "Summary":" ",
   "Link":" "},
+  
   {"id":3,
   "Title":"Air-Quality India EDA",
+   "Summary":" ",
   "Link":" "},
-  {"id":1,
+  
+  {"id":4,
   "Title":"Elo Customer Segmentation using XGBoost",
+   "Summary":" ",
   "Link":" "}  
 ]
 
@@ -20,9 +26,11 @@ Certificates =[
   {"id":1,
   "Title":"DataScience and Machine Learning Bootcamp",
   "Link":" "},
+  
   {"id":2,
   "Title":"Machine Learning with Python",
   "Link":" "},
+  
   {"id":3,
   "Title":"Deep Learning with Pytorch",
   "Link":" "}
@@ -30,8 +38,11 @@ Certificates =[
 
 @app.route("/")
 def html_webpage():
-  return render_template('home.html', projects="Projects", certificates ="Certificates")
-  print(__name__)
+  return render_template('home.html', projects=Projects, certificates =Certificates)
+
+@app.route("/api/projects")
+def load_projects():
+  return jsonify(Projects+Certificates)
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug = True)
